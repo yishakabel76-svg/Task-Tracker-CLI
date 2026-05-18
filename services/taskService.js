c6837c6ac5f3886998a14ed6163b5225d554c156
Yeshak Abel <yishakabel76@gmail.com>
2026-05-18 16:48:48 +0700

    update

--- services/taskService.js
+++ services/taskService.js
@@ -10,3 +10,26 @@ function AddTask(title) {
     writeTasks(tasks);
     return newTask;
 }
+function updateTask(id, title) {
+    const tasks = readTasks();
+
+    const task = tasks.find(t => t.id === id);
+    if(!task)return null;
+    task.title = title;
+    writeTasks(tasks);
+    return task;
+}
+function deleteTask(id){
+    let tasks = readTasks()
+    tasks = tasks.filter(t =>  t.id !== id);
+    writeTasks(tasks)
+
+}
+function markDone(id){
+    const tasks = readTasks()
+    const task = tasks.find(t => t.id === id);
+    if(!tasks)return null;
+    task.status = "done";
+    writeTasks(tasks);
+    return task
+}
